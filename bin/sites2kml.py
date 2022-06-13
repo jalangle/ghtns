9d4b9eafbbdca083f59bcb46a7c1b95be94b10c8
Jacob Langley <jacob.langley@ghtns.com>
2022-06-13 15:59:04 -0700

    Add a script to convert site notes to a KML file

---
+++ bin/sites2kml.py
@@ -0,0 +1,53 @@
+#!python3
+
+from datetime import date
+import frontmatter
+import os
+from pathlib import Path
+import simplekml
+
+class Location:
+	def __init__(self, name, latitude, longitude):
+		self.Name = name
+		self.Latitude = latitude
+		self.Longitude = longitude
+
+	def __lt__(self, other):
+		if(self.Latitude < other.Latitude):
+			return True
+		if(self.Latitude > other.Latitude):
+			return False
+		return self.Longitude < other.Longitude
+
+	def __eq__(self, other):
+		return self.Latitude == other.Latitude and self.Longitude == other.Longitude
+
+def GetLocations():
+	""" Retrieve Locations from markdown frontmatter annotations"""
+	locations = list()
+
+	# Load locations from markdown
+	p = Path('photosites')
+	for d in p.iterdir() :
+		print("Loading on " + str(d))
+		with open(d) as f:
+			site = frontmatter.load(f)
+			l = Location(site['Title'], site['Location']['Coordinates']['Latitude'], site['Location']['Coordinates']['Longitude'])
+			locations.append(l)
+
+	# sort the list of locations (sort is by Lat, then Long) so that they appear in a consistent order
+	locations.sort(reverse=True)
+	return locations
+
+def LocationsToKML(locations):
+	""" Convert locations to a KML file """
+	sites = simplekml.Kml(name="Location Site Impressions, " + date.today().strftime("%Y/%m/%d"))
+	for l in locations:
+		sites.newpoint(name=l.Name, coords=[(l.Longitude, l.Latitude)])
+	sites.save("photosites/sites.kml")
+
+def main(): 
+	locations = GetLocations()
+	LocationsToKML(locations)
+
+main()
